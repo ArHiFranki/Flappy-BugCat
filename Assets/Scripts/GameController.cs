722fa733233c15af8e44a4e3a8ab95ff42b5744b
ArHiFranki <franki550@gmail.com>
2021-11-14 17:10:11 +0700

    Отредактировал параметры сцены, создал заготовку для GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private PipeGenerator _pipeGenerator;
+
+
+}
