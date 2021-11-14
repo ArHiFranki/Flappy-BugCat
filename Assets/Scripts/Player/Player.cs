02ba58f3088bebcde104ca009e7ca6047edcf86f
ArHiFranki <franki550@gmail.com>
2021-11-14 20:09:16 +0700

    Добавил ассеты из предыдущей версии

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMover))]
+public class Player : MonoBehaviour
+{
+    private PlayerMover _mover;
+    private int _score;
+
+    private void Start()
+    {
+        _score = 0;
+        _mover = GetComponent<PlayerMover>();
+    }
+
+    public void AddScore()
+    {
+        _score++;
+        Debug.Log("Score:" + _score);
+    }
+
+    public void ResetGame()
+    {
+        _score = 0;
+        _mover.ResetPlayer();
+        Time.timeScale = 1;
+    }
+
+    public void Die()
+    {
+        Debug.Log("Game Over!");
+        Time.timeScale = 0;
+    }
+}
